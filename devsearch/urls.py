d721fae566b0b675e7c56193e657610ac4a83ca4
Mahesh <maheshchaudhari3322@gmail.com>
2025-11-24 02:07:03 +0530

    starting with static files

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls'))
 ]
+
+#appending url paths for media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#MEDIA_ROOT is where we send user uploaded content & MEDIA_URL to saccess that content.
