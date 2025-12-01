bfe1f489c8a54e9a1737f3ccd2bea7519396651e
Mahesh <maheshchaudhari3322@gmail.com>
2025-12-01 12:18:30 +0530

    Theme Installation – Final Clean Version

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -12,3 +12,6 @@ urlpatterns = [
 #appending url paths for media files
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #MEDIA_ROOT is where we send user uploaded content & MEDIA_URL to saccess that content.
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#STATIC_ROOT is where all static files will be collected when we run collectstatic command 
+#in production & STATIC_URL to access those static files. DEBUG = FALSE
--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):  #it is just generating form based on model fields in viewspy.
     class Meta:
         model = Project
-        fields = ['title', 'description', 'demo_link', 'demo_link', 'source_link', 'tags']
+        fields = ['title', 'featured_image', 'description', 'demo_link', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        } #to show tags as checkbox in form instead of default select multiple widget.
+
+        def __init__(self, *args, **kwargs): #customizing form fields appearance
+            super(ProjectForm, self).__init__(*args, **kwargs)
+            
+            for name, field in self.fields.items(): #iterating through all fields in the form eg:title, description  
+                field.widget.attrs.update({'class': 'input input--text'}) 
