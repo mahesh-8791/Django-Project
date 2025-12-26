89ca027391cb99ccc91a099df4fa4810de97a133
Mahesh <maheshchaudhari3322@gmail.com>
2025-12-26 19:24:06 +0530

    Added & Rendered Files

--- users/views.py
+++ users/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
 def profiles(request):
-    return render(request, 'users/profiles.html')
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk) # Get profile by primary key
+
+    topSkills = profile.skill_set.exclude(description__exact="") #profile--child obj, skill--child model
+    #if skill dont have description then exclude it from topSkills
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills} # Pass profile to context
+    return render(request, 'users/user-profile.html', context)
